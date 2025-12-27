d0360dc52ce333a9414d98b0021eb9d5dbd8bafd
Danila <danilamatush72@gmail.com>
2025-12-27 20:15:27 +0300

    feat(infrastructure): add UserContext

---
+++ src/InternetMarket.UserService/InternetMarket.UserService.Infrastructure/Persistence/DB/UserContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternetMarket.UserService.Infrastructure.Persistence.DB
+{
+    public class UserContext
+    {
+        
+    }
+}
