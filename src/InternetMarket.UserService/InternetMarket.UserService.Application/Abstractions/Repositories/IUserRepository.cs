5133a3b53f58c264cdb959d05a5f7299bdae8da0
Danila <danilamatush72@gmail.com>
2025-12-27 20:31:06 +0300

    refactor(domain): change argument from int to string

--- src/InternetMarket.UserService/InternetMarket.UserService.Application/Abstractions/Repositories/IUserRepository.cs
+++ src/InternetMarket.UserService/InternetMarket.UserService.Application/Abstractions/Repositories/IUserRepository.cs
@@ -1,16 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using InternetMarket.UserService.Domain;
 
 namespace InternetMarket.UserService.Application.Abstractions.Repositories
 {
-    public interface UserRepository
+    public interface IUserRepository
     {
         public Task<IEnumerable<User>> GetAllAsync();
         public Task<User?> GetByIdAsync(Guid Id);
-        public Task<User?> GetByEmailAsync(Guid Id);
+        public Task<User?> GetByEmailAsync(string Email);
         public Task CreateAsync(User user);
         public Task UpdateAsync(User user);
         public Task DeleteAsync(User user);
