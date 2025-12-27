fb76353971402c14a60cec5fa0df1b3d4c33c881
Danila <danilamatush72@gmail.com>
2025-12-27 20:32:52 +0300

    feat(infrastructure): add UserContext

--- src/InternetMarket.UserService/InternetMarket.UserService.Infrastructure/Persistence/DB/UserContext.cs
+++ src/InternetMarket.UserService/InternetMarket.UserService.Infrastructure/Persistence/DB/UserContext.cs
@@ -1,12 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using InternetMarket.UserService.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace InternetMarket.UserService.Infrastructure.Persistence.DB
 {
-    public class UserContext
+    public class UserContext(DbContextOptions<UserContext> options) : DbContext(options)
     {
-        
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
